385a36c38228993e79518fa6cede3d4ec6e43135
Tisma <matisagr@gmail.com>
2025-11-19 17:28:24 +0100

    Refactor code structure for improved readability and maintainability

--- 01_data_clean.R
+++ 01_data_clean.R
@@ -3,20 +3,35 @@ library(lubridate)
 
 setwd("s:/Bureau/Ceci est un dossier (SSD)/IPSSI Cours/R/Projet_VideoGames")
 
-df_raw <- read.csv("games.csv", stringsAsFactors = FALSE, encoding = "UTF-8")
+# charger le csv
+df_raw <- read_csv("games.csv", show_col_types = FALSE)
 
 cat("Données brutes :", nrow(df_raw), "lignes\n")
-cat("Colonnes du fichier :\n")
-print(colnames(df_raw)[1:15])
 
 df_clean <- df_raw %>%
+  # normaliser
+  rename(
+    Release.date = `Release date`,
+    Supported.languages = `Supported languages`,
+    Full.audio.languages = `Full audio languages`,
+    Metacritic.score = `Metacritic score`,
+    User.score = `User score`,
+    Average.playtime.forever = `Average playtime forever`
+  ) %>%
+  
+  # vérifier que AppID est bien un nombre. 
+  filter(str_detect(as.character(AppID), "^[0-9]+$")) %>%
+  
   distinct() %>%
   select(AppID, Name, Release.date, Price,
          Supported.languages, Full.audio.languages, Windows, Mac, Linux,
          Metacritic.score, User.score, Positive, Negative,
          Average.playtime.forever, Developers, Publishers, Categories) %>%
   mutate(
-    Release.date = as.Date(Release.date, format = "%b %d, %Y"),
+    # formatage par sécurité
+    Release.date = parse_date_time(Release.date, orders = c("b d, Y", "Y-m-d", "b Y")),
+    Release.date = as.Date(Release.date),
+    
     Price = as.numeric(Price),
     Metacritic.score = as.numeric(Metacritic.score),
     User.score = as.numeric(User.score),
@@ -27,8 +42,6 @@ df_clean <- df_raw %>%
   filter(Price > 0 | is.na(Price))
 
 cat("Données nettoyées :", nrow(df_clean), "lignes\n")
-cat("Colonnes conservées :", ncol(df_clean), "\n")
 
+# réécrire un nouveau fichier csv cleané
 write.csv(df_clean, "games_cleaned.csv", row.names = FALSE)
-
-cat("Fichiers sauvegardés : games_cleaned.csv\n")
